e3941bc9d0298db767da6858d812f326feeab34e
Bercant <efecanberber3@gmail.com>
2026-09-08 15:23:44 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hukuk50k-v49';
+const CACHE='nexora-v52';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./cloud-config.js','./cloud.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
